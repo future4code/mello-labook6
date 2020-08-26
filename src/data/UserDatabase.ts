11bdfa36001a4afe064ad80e5e78aa16e21321bb
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-08-26 15:36:07 -0300

    Endpoints 2 e 6

--- src/data/UserDatabase.ts
+++ src/data/UserDatabase.ts
@@ -3,7 +3,37 @@ import { BaseDatabase } from "./BaseDatabase";
 
 export class UserDatabase extends BaseDatabase {
 
-  private static TABLE_NAME = "";
+  private static TABLE_NAME = "Users";
+
+  public async signIn (email: string): Promise<any> {
+    try {
+      const result = await super.getConnection().raw(`
+        SELECT * FROM ${UserDatabase.TABLE_NAME} WHERE email = "${email}"
+      `);
+      return result;
+    } catch (error) {
+      throw new Error (error.message)
+    } finally {
+      super.destroyConnection();
+    }
+  }
+
+  public async feed (id: string): Promise<any> {
+    try {
+      const result = await super.getConnection().raw(`
+              SELECT Post.id, text, create_at, id_user, type, name FROM Post
+              JOIN Users ON Users.id = Post.id_user
+              JOIN Followers ON Followers.idUser = Users.id
+              WHERE Followers.idFollower = "${id}"
+              ORDER BY Post.create_at DESC;
+      `);
+      return result;
+    } catch (error) {
+      throw new Error (error.message)
+    } finally {
+      await super.destroyConnection();
+    }
+  }
 
  
 
