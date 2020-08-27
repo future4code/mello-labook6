88474b328694394ba9c87debdc15df73bffa91b4
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-08-27 00:36:54 -0300

    adjusts endpoints

--- src/data/UserDatabase.ts
+++ src/data/UserDatabase.ts
@@ -2,15 +2,42 @@ import { BaseDatabase } from "./BaseDatabase";
 
 
 export class UserDatabase extends BaseDatabase {
-  feed(id: string) {
-      throw new Error("Method not implemented.");
-  }
 
   private static TABLE_NAME: string = "User";
 
   public async registerUser(id: string, username: string, email: string, password: string): Promise<void> {
     await this.getConnection()
       .insert({id, username, email, password }).into(UserDatabase.TABLE_NAME);
+  }
+
+  public async signIn (email: string): Promise<any> {
+    try {
+      const result = await super.getConnection().raw(`
+        SELECT * FROM ${UserDatabase.TABLE_NAME} WHERE email = "${email}"
+      `);
+      return result;
+    } catch (error) {
+      throw new Error (error.message)
+    } finally {
+      super.destroyConnection();
+    }
+  }
+
+  public async feed (id: string): Promise<any> {
+    try {
+      const result = await super.getConnection().raw(`
+              SELECT Post.id, text, create_at, id_user, type, name FROM Post
+              JOIN Users ON Users.id = Post.id_user
+              JOIN Followers ON Followers.idUser = Users.id
+              WHERE Followers.idFollower = "${id}"
+              ORDER BY Post.create_at DESC;
+      `);
+      return result;
+    } catch (error) {
+      throw new Error (error.message)
+    } finally {
+      await super.destroyConnection();
+    }
   }
 
 }
